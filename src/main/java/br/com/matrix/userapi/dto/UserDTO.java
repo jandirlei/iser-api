ec51f1b632d564521c1f9c480ce639e6f383c8d4
jandirlei <jandirlei@yahoo.com.br>
2023-08-20 22:29:45 -0300

    Classe User em construção

--- src/main/java/br/com/matrix/userapi/dto/UserDTO.java
+++ src/main/java/br/com/matrix/userapi/dto/UserDTO.java
@@ -1,5 +1,32 @@
 package br.com.matrix.userapi.dto;
 
-public class UserDTO {
+import java.sql.Date;
+import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+	
+	@NotBlank(message = "Nome é obrigatório")
+    private String nome;
+	
+	@NotBlank(message = "CPF é obrigatório")
+    private String cpf;
+	
+    private String endereco;
+    
+    @NotBlank(message = "E-mail é obrigatório")
+    private String email;
+    
+    private String telefone;
+    
+    private LocalDateTime dataCadastro;
+ 
+    private String key;
 }
---
+++ src/main/java/br/com/matrix/userapi/model/User.java
@@ -0,0 +1,46 @@
+package br.com.matrix.userapi.model;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+import br.com.matrix.userapi.dto.UserDTO;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+//@Entity
+//@Table
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+    private String key;
+
+    public static User convert(final UserDTO userDTO) {
+        User user = new User();
+        user.setNome(userDTO.getNome());
+        user.setEndereco(userDTO.getEndereco());
+        user.setCpf(userDTO.getCpf());
+        user.setEmail(userDTO.getEmail());
+        user.setTelefone(userDTO.getTelefone());
+        user.setDataCadastro(userDTO.getDataCadastro()); 
+        user.setKey(userDTO.getKey());
+        return user;
+    }
+}
